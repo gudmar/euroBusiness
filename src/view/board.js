d399cef717e6470cc2348eae7536651aa3211e17
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-04-09 03:13:20 +0200

    board view added

---
+++ src/view/board.js
@@ -0,0 +1,225 @@
+
+const boardFieldView = {
+            Start: {
+                width: 2,
+                color: none,
+                icon: 'ArrowBack',
+                title: 'big',
+                direction: '-45deg',
+            },
+            Ateny: {
+                width: 1,
+                color: yellow,
+                icon: none,
+                direction: '0deg',
+            },
+            Chance_blue: {
+                color: blue,
+                width: 1,
+                title: 'huge'
+            },
+            Chance_red: {
+                color: red,
+                width: 1,
+                title: 'huge',
+                icon: 'QuestionMark'
+            },
+            Saloniki: {
+                width: 1,
+                color: yellow,
+                icon: none,
+                direction: '0deg',
+            },
+            Guarded_Parking: {
+                color: red,
+                width: 1,
+                title: 'huge',
+                icon: 'LocalParking',
+            },
+            Free_Parking: {
+                color: none,
+                width: 2, 
+                direction: '135deg',
+                icon: 'DirectionsCar'
+            },
+            Jail: {
+                color: none,
+                width: 2,
+                direction: '225deg',
+                icon: 'HouseSiding'
+            },
+            Go_to_jail: {
+                direction: '225deg',
+                width: 2,
+                color: none,
+                symbol: 'U+1F6A8'
+            },
+            South_Railways: {
+                color:none,
+                direction: '0deg',
+                icon: Train,
+                width: 1,
+            },
+            Neapol: {
+                width: 1,
+                color: 'red',
+                icon: none,
+                direction: '0deg',
+            },
+            Mediolan: {
+                width: 1,
+                color: 'red',
+                icon: none,
+                direction: '0deg',
+            },
+            Rome: {
+                width: 1,
+                color: 'red',
+                icon: none,
+                direction: '0deg',
+            },
+            Barcelona: {
+                width: 1,
+                color: 'blue',
+                icon: none,
+                direction: '90deg',
+            },
+            Power_Station: {
+                width: 1,
+                color: none,
+                icon: 'LightBulb',
+                direction: '90deg',
+
+            },
+            Sewilla: {
+                width: 1,
+                color: 'blue',
+                icon: none,
+                direction: '90deg',
+
+            },
+            Madrit: {
+                width: 1,
+                color: 'blue',
+                icon: none,
+                direction: '90deg',
+
+            },
+            West_Railways: {
+                color:none,
+                direction: '90deg',
+                icon: Train,
+                width: 1,
+            },
+            Liverpool: {
+                width: 1,
+                color: 'orange',
+                icon: none,
+                direction: '90deg',
+            },
+            Glasgow: {
+                width: 1,
+                color: 'orange',
+                icon: none,
+                direction: '90deg',
+            },
+            Londyn: {
+                width: 1,
+                color: 'orange',
+                icon: none,
+                direction: '90deg',
+            },
+            Rotterdam: {
+                width: 1,
+                color: 'green',
+                icon: none,
+                direction: '180deg',
+            },
+            Bruksela: {
+                width: 1,
+                color: 'green',
+                icon: none,
+                direction: '180deg',
+            },
+            Amsterdam: {
+                width: 1,
+                color: 'green',
+                icon: none,
+                direction: '180deg',
+            },
+            North_Railways: {
+                color:none,
+                direction: '180deg',
+                icon: Train,
+                width: 1,
+            },
+            Malmo: {
+                width: 1,
+                color: 'purple',
+                icon: none,
+                direction: '180deg',
+            },
+            Goteborg: {
+                width: 1,
+                color: 'purple',
+                icon: none,
+                direction: '180deg',
+
+            },
+            Water_plant: {
+                width: 1,
+                color: none,
+                icon: 'Wash',
+                direction: '180deg',
+
+            },
+            Sztokholm: {
+                width: 1,
+                color: 'purple',
+                icon: none,
+                direction: '180deg',
+
+            },
+            Frankfurt: {
+                width: 1,
+                color: 'brown',
+                icon: none,
+                direction: '270deg',
+            },
+            Kolonia: {
+                width: 1,
+                color: 'brown',
+                icon: none,
+                direction: '270deg',
+            },
+            Bonn: {
+                width: 1,
+                color: 'brown',
+                icon: none,
+                direction: '270deg',
+            },
+            East_Railways: {
+                color:none,
+                direction: '270deg',
+                icon: Train,
+                width: 1,
+            },
+            Insbruck: {
+                width: 1,
+                color: 'black',
+                icon: none,
+                direction: '270deg',
+            },
+            Tax: {
+                type: 'tax',
+                visit: [200]
+            },
+            Wieden: {
+                width: 1,
+                color: 'black',
+                icon: none,
+                direction: '270deg',
+            },
+        };
+
+export default boardFieldView;
