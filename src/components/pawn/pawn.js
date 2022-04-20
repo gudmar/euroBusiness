f9f4663db75a57ac589e04a341b4daabf8da5a62
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-04-20 02:07:07 +0200

    Pawn moving works

--- src/components/pawn/pawn.js
+++ src/components/pawn/pawn.js
@@ -2,18 +2,25 @@ import styles from './pawn.module.css';
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useEffect, useState, useRef } from 'react';
+import { move, setDiceResult, moveOneField } from '../../state/playerSlice';
 
 const Pawn = props => {
     const player = useSelector(state => state.playerSlice[props.color]);
     const board = useSelector(state => state.boardSlice)
     const fieldNumber = useSelector(state => state.playerSlice[props.color].fieldNumber);
+    const diceResult = useSelector(state => state.playerSlice.diceResult);
+    const currentPlayer = useSelector(state => state.playerSlice.currentPlayer);
     const store = useStore();
     const pawn = useRef();
     const spacing = 5;
+    const dispatch = useDispatch();
     const getMiddleCords = (fieldNr) => {
         try {
             const { left, right, top, bottom } = board.fieldDescriptors[fieldNr];
             const average = (a, b) => (a + b) / 2;
+            const x = average(left, right);
+            const y = average(top, bottom)
+            if (player.color === 'blue') console.log(fieldNr, left, right, top ,bottom, x, y)
             return {x: average(left, right), y: average(top, bottom)}
         }
         catch(e){
@@ -60,6 +67,24 @@ const Pawn = props => {
     }
     const [cords, setCords] = useState();
     useEffect(() => {
+        
+        if (diceResult > 0) {  
+            const t = setTimeout(() => {
+                console.log(diceResult)
+                const nextField = fieldNumber + 1;
+                if (nextField > 40) nextField = 0;
+                // dispatch(move({player: currentPlayer, nrOfFields: nextField}));
+                // dispatch(setDiceResult([diceResult - 1, 0]));
+                console.log(currentPlayer)
+                dispatch(moveOneField(currentPlayer));
+                console.log(diceResult, nextField)
+                clearTimeout(t);
+            }, 1000);
+        }
+    }, [diceResult]);
+
+    useEffect(() => {
+        if (player.color === 'blue') console.log(fieldNumber)
         if (board.fieldDescriptors.length > 0) {
             setCords(getMiddleCords(fieldNumber))
         }
