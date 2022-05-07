f3ee8602df53ca2f74c9822211795f8aa13b7c6f
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-05-07 02:26:53 +0200

    tests with attempt to mock an internal function

--- src/state/__test__/boardFields.test.js
+++ src/state/__test__/boardFields.test.js
@@ -1,7 +1,15 @@
 import testState from './stateForTests.js'
-import { getNrOfCitiesPlayerHas } from '../boardFields.js'
+import { 
+    getNrOfCitiesPlayerHas,
+    // throwDices,
+    countWaterPlantVisitFee,
+} from '../boardFields.js'
+
+const throwDice = require('../../functions/throwDices.js');
+
 
 const cp = state => JSON.parse(JSON.stringify(state));
+
 
 describe('boardFields: getNrOfCitiesPlayerHas', () => {
     //descriptors, player, country
@@ -24,5 +32,41 @@ describe('boardFields: getNrOfCitiesPlayerHas', () => {
         const expected = {owns: 4, outOf: 4};
         expect(result).toEqual(expected);
     })
+})
 
+describe('boardFields: countWaterPlantVisitFee', () => {
+    const getWaterPlant = descriptors => descriptors.Water_Plant;
+    it('Should return 0 if bank is the owner of waterPlant', async () => {
+        const state = cp(testState);
+        const waterPlant = getWaterPlant(state);
+        const result = await countWaterPlantVisitFee(state, waterPlant);
+        const expected = 0;
+        expect(result).toBe(expected);
+    })
+    it('Should return 0 in case object is plegded', async () => {
+        const state = cp(testState);
+        const waterPlant = getWaterPlant(state);
+        waterPlant.isPlegded = true;
+        waterPlant.owner = 'Bolek';
+        const result = await countWaterPlantVisitFee(state, waterPlant);
+        const expected = 0;
+        expect(result).toBe(expected);        
+    })
+    it('Should return 50 in case 5 is thrown and owner has only one object', async () => {
+            const throwDices = jest.spyOn(throwDice, 'throwDices').mockImplementation(async () => 5)
+            throwDice.throwDices = jest.spyOn(throwDice, 'throwDices').mockImplementation(async () => 5)
+            console.log('TH DICES', await throwDices())
+            console.log('TH DICES', await throwDices())
+            console.log('TH DICES', await throwDices())
+            console.log('TH DICES', await throwDices())
+            const state = cp(testState);
+            const waterPlant = getWaterPlant(state);
+            waterPlant.owner = 'Bolek';
+            const result = await countWaterPlantVisitFee(state, waterPlant);
+            const expected = 50;
+            expect(result).toBe(expected);        
+    })
+    it('Should return 100 in case 5 is thrown and owner has both: power station and water plant', () => {
+
+    })
 })
