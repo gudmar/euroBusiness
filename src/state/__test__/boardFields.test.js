3cc8dab396a4240a052a52b5f9c5f15f9c0747eb
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-05-08 01:28:25 +0200

    Tests to countCityVisitFee added

--- src/state/__test__/boardFields.test.js
+++ src/state/__test__/boardFields.test.js
@@ -83,7 +83,7 @@ describe('boardFields: countWaterPlantVisitFee', () => {
         const expected = 100;
         expect(result).toBe(expected);
     })
-    it('Should return 50 in case 5 is thrown and owner has both: power station and water plant but the other estate is plegded', async () => {
+    it('Should return 100 in case 5 is thrown and owner has both: power station and water plant but the other estate is plegded', async () => {
         const state = cp(testState)
         const powerStation = getPowerStation(state);
         const waterPlant = getWaterPlant(state);
@@ -92,7 +92,7 @@ describe('boardFields: countWaterPlantVisitFee', () => {
         waterPlant.isPlegded = true;
         const throwDices = jest.spyOn(throwDice, 'throwDices').mockImplementation(async () => 5);
         const result = await bf.countWaterPlantVisitFee(state, powerStation);
-        const expected = 50;
+        const expected = 100;
         expect(result).toBe(expected);
     })
 })
@@ -126,7 +126,7 @@ describe('boardField: asumpWaterPlantVisitFee ', () => {
         const expected = '10 x dice throw result';
         expect(result).toBe(expected);
     });
-    it('Should return "10 dice throw result" in case power station and water plant are owned by the same user, but other field is plegded', () => {
+    it('Should return "20 dice throw result" in case power station and water plant are owned by the same user, but other field is plegded', () => {
         const state = cp(testState)
         const powerStation = getPowerStation(state);
         const waterPlant = getWaterPlant(state);
@@ -134,7 +134,7 @@ describe('boardField: asumpWaterPlantVisitFee ', () => {
         powerStation.owner = 'Lolek';
         waterPlant.isPlegded = true;
         const result = asumpWaterPlantVisitFee(state, powerStation);
-        const expected = '10 x dice throw result';
+        const expected = '20 x dice throw result';
         expect(result).toBe(expected);
     });
     it('Should return "20 dice throw result" in case power station and water plant are owned by the same user', () => {
@@ -147,5 +147,112 @@ describe('boardField: asumpWaterPlantVisitFee ', () => {
         const expected = '20 x dice throw result';
         expect(result).toBe(expected);        
     })
+})
 
+describe('boardFields: countCityVisitFee', () => {
+    const getLiverpool = state => state.Liverpool;
+    const getLondon = state => state.London;
+    const getGlasgow = state => state.Glasgow;
+    let Liverpool, Glasgow, London = undefined;
+    let state = undefined;
+    const setVariables = () => {
+        state = cp(testState);
+        Liverpool = getLiverpool(state);
+        Glasgow = getGlasgow(state);
+        London = getLondon(state);
+    }
+    it('Should throw and error in case any city of certain country is plegded or not owned by player, and there are houses', () => {
+        // const state = cp(testState);
+        // const Liverpool = getLiverpool(state);
+        // const Glasgow = getGlasgow(state);
+        // const London = getLondon(state);
+        setVariables();
+        Liverpool.owner = 'Lolek';
+        Liverpool.nrOfHouses = 2;
+        London.owner = 'Lolek';
+        Glasgow.owner = 'Lolek';
+        Glasgow.isPlegded = true;
+        const result = () => countCityVisitFee(state, Liverpool);
+        expect(result).toThrow();
+    });
+    it('Should return 30 in case of Liverpool owned by player A and rest of UK owned by other player, and Loverpool not plegded', () => {
+        // const state = cp(testState);
+        // const Liverpool = getLiverpool(state);
+        // const Glasgow = getGlasgow(state);
+        // const London = getLondon(state);
+        setVariables();
+        Liverpool.owner = 'Lolek';
+        London.owner = 'bank';
+        Glasgow.owner = 'bank';
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(30);
+    })
+    it('Should return 0 in case target city is plegded', () => {
+        setVariables();
+        Liverpool.owner = 'Lolek';
+        Liverpool.isPlegded = true;
+        London.owner = 'Lolek';
+        Glasgow.owner = 'Lolek';
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(0);
+    })
+    it('Should return 30 in case Liverpool and all UK except for London are owned by player A, and London belongs to some other player', () => {
+        setVariables();
+        Liverpool.owner = 'Lolek';
+        Liverpool.isPlegded = true;
+        London.owner = 'Homes';
+        Glasgow.owner = 'Lolek';
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(0);
+    });
+    it('Should return 60 in case a player stepped on Liverpool all cities of UK are owned by player A, but London is plegded', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        London.isPlegded = true;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(60);
+    })
+    it('Should return 60 in case a player stepped on Liverpool all citeis of UK are ownde by A and there are no houses on Liverpool', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        London.nrOfHouses = Glasgow.nrOfHouses = 1;
+        Liverpool.nrOfHouses = 0;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(60)
+    })
+    it('Should return 140 in case a player stepped on Liverpool with one house', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        Liverpool.nrOfHouses = London.nrOfHouses = Glasgow.nrOfHouses = 1;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(140)
+    })
+    it('Should return 400 in case a player stepped on Liverpool with 2 houses', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        Liverpool.nrOfHouses = London.nrOfHouses = Glasgow.nrOfHouses = 2;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(400)
+    })
+    it('Should return 1100 in case a player stepped on Liverpool with 2 houses', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        Liverpool.nrOfHouses = London.nrOfHouses = Glasgow.nrOfHouses = 3;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(1100)
+    })
+    it('Should return 1500 in case a player stepped on Liverpool with 2 houses', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        Liverpool.nrOfHouses = London.nrOfHouses = Glasgow.nrOfHouses = 4;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(1500)
+    })
+    it('Should return 1900 in case a player stepped on Liverpool with one hotel', () => {
+        setVariables();
+        Liverpool.owner = London.owner = Glasgow.owner = 'Bolek';
+        Liverpool.nrOfHouses = London.nrOfHouses = Glasgow.nrOfHouses = 5;
+        const result = countCityVisitFee(state, Liverpool);
+        expect(result).toBe(1900)
+    })
 })
